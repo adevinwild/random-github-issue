a10504ea10f786d2c1c82c0bfde676aca5417489
Adil <abasri@webexpr.dev>
2024-11-30 00:58:28 +0100

    chore: Removed console log

--- src/app/[...repo]/page.tsx
+++ src/app/[...repo]/page.tsx
@@ -22,10 +22,6 @@ export default async function IssuePage({ params }: { params: { repo: string[] }
   const issue = result?.issue
   const expiresAt = result?.expiresAt
 
-
-
-  console.log(expiresAt)
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
